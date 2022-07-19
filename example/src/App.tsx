4ce960d83e922dd4e9e1b5c4180577ed00e14916
Chandravardhan8998 <chandravardhansingh2@gmail.com>
2022-07-19 21:55:54 +0530

    feat: animated header for scrollview

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,37 +1,81 @@
 /* eslint-disable prettier/prettier */
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 import { HeaderScrollView } from 'react-native-scroll-header';
 
-const _renderHeaderContent = () => <View style={styles.headerContent} />;
+const headerImage = 'https://c4.wallpaperflare.com/wallpaper/611/838/413/spiderman-hd-4k-superheroes-wallpaper-preview.jpg'
+const headerImage2 = 'https://c4.wallpaperflare.com/wallpaper/857/67/782/spider-man-homecoming-2017-wallpaper-preview.jpg'
+const headerImage3 = 'https://c4.wallpaperflare.com/wallpaper/576/838/565/spiderman-4k-artwork-hd-wallpaper-preview.jpg'
+
+const _renderHeaderContent = () => <View style={styles.headerContent} >
+  <Text
+    style={styles._renderHeaderContent}
+  >Watch full movie on Disney+Hotstar{'\n'}</Text>
+</View>;
+const _renderScrollHeader = () => {
+  return <View
+    style={styles.renderScrollHeader}
+  >
+    <Text
+      style={styles.headerTitle}
+    >Spider Man</Text>
+  </View>
+}
 export default function App() {
   return (
     <HeaderScrollView
       renderHeaderContent={_renderHeaderContent}
       image={{
-        uri: "https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        uri: headerImage
       }}
+      renderScrollHeader={_renderScrollHeader}
     >
-      {[...new Array(20)].map((_, i) => {
-        return <View
-          key={i}
-          style={styles.borderBox}
-        />
-      })}
+      <Image
+        source={{
+          uri: headerImage2
+        }}
+        style={styles.flexImage}
+      />
+      <Image
+        source={{
+          uri: headerImage3
+        }}
+        style={styles.flexImage}
+      />
     </HeaderScrollView>
   );
 }
 const styles = StyleSheet.create({
   headerContent: {
     width: "100%",
-    height: 80,
-    backgroundColor: "#ff000050"
+    height: 100,
+    backgroundColor: "#e0b02b"
   },
   borderBox: {
     height: 100,
     width: '100%',
-    backgroundColor: "#171717",
-    borderBottomWidth: 2,
-    borderBottomColor: '#fff'
+    backgroundColor: "#cb3618",
+  },
+  _renderHeaderContent: {
+    fontWeight: "700",
+    fontSize: 14,
+    color: "#5d6e97",
+    padding: 10
+  },
+  renderScrollHeader: {
+    height: 70,
+    width: Dimensions.get('screen').width,
+    backgroundColor: "#872b2f",
+    justifyContent: "center", alignItems: "center"
+  },
+  headerTitle: {
+    fontWeight: "700",
+    fontSize: 20,
+    color: "#fff"
+  },
+  flexImage: {
+    height: 500,
+    width: '100%',
+    resizeMode: "cover"
   }
 })
--- src/scroll/AnimatedScroll.tsx
+++ src/scroll/AnimatedScroll.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import {
     Animated,
     Dimensions,
@@ -11,17 +11,22 @@ import {
     NativeSyntheticEvent,
     ViewStyle,
     ScrollViewProps,
+    SafeAreaView,
+    StatusBar,
 } from 'react-native';
 
-const { width } = Dimensions.get('screen');
+const { width } = Dimensions.get('window');
 type Props = {
     image: ImageSourcePropType;
     imageStyle?: ImageStyle;
     containerStyle?: ViewStyle;
     children: JSX.Element | JSX.Element[];
     renderHeaderContent?: () => JSX.Element | JSX.Element[];
+    renderScrollHeader?: () => JSX.Element | JSX.Element[] | null;
+
 };
 const inputValue = [1, 1.5, 2];
+
 const ModalInfo = ({
     children,
     image,
@@ -32,10 +37,11 @@ const ModalInfo = ({
         resizeMode: 'cover',
     },
     renderHeaderContent = () => <></>,
+    renderScrollHeader = () => null,
     ...props
 }: Props & ScrollViewProps) => {
     const animatedValue = new Animated.Value(1);
-    // const animatedValue = useRef(new Animated.Value(1)).current
+    const headerRef = useRef<View>()
     console.log(animatedValue);
 
     const imageScale = animatedValue.interpolate({
@@ -46,79 +52,60 @@ const ModalInfo = ({
     const borderRadiusValue = animatedValue.interpolate({
         inputRange: inputValue,
         outputRange: [30, 15, 0],
+        extrapolate: "clamp"
     });
-    // const translateLeft = animatedValue.interpolate({
-    //     inputRange: [1, 1.1],
-    //     outputRange: [1, -40],
-    // });
-    // const translateRight = animatedValue.interpolate({
-    //     inputRange: [1, 1.1],
-    //     outputRange: [1, 40],
-    // });
     const gravityDown = animatedValue.interpolate({
         inputRange: [1, 1.1],
         outputRange: [1, 100],
+        extrapolate: "clamp"
     });
     let scrollViewRef = useRef();
     const _onScrollHandler = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-        if (e.nativeEvent.contentOffset.y / width + 1 < 3) {
-            animatedValue.setValue(e.nativeEvent.contentOffset.y / width + 1);
+        const scrollYPositionByWidth = e.nativeEvent.contentOffset.y / width + 1
+        if (scrollYPositionByWidth < 3) {
+            if (scrollYPositionByWidth > 2) {
+            }
+            animatedValue.setValue(scrollYPositionByWidth);
         }
     };
+    const animatedHeaderStyleMemo = useMemo(() => animatedHeaderStyle(borderRadiusValue, containerStyle), [borderRadiusValue, containerStyle])
+    const allowStickyHeader = useMemo(() => renderScrollHeader() ? [1] : undefined, [renderScrollHeader])
+    const gravityDownHeaderStyleMemo = useMemo(() => gravityDownHeaderStyle(gravityDown), [gravityDown])
+    const mainHeaderImageStyleMemo = useMemo(() => mainHeaderImageStyle(imageStyle, imageScale), [imageStyle, imageScale])
     return (
-        <>
-            <Animated.View style={styles.imageContainerView}>
-                {!!image && <Animated.Image
+        <SafeAreaView
+            style={styles.safeAreaView}
+        >
+            {!!image && <Animated.View style={styles.imageContainerView}>
+                <Animated.Image
                     source={image}
-                    style={[
-                        imageStyle,
-                        {
-                            transform: [
-                                {
-                                    scale: imageScale,
-                                },
-                            ],
-                        },
-                    ]}
-                />}
+                    style={mainHeaderImageStyleMemo}
+                />
                 <Animated.View
-                    style={[
-                        styles.renderHeaderContainer,
-                        {
-                            transform: [
-                                {
-                                    translateY: gravityDown,
-                                },
-                            ],
-                        },
-                    ]}
+                    style={gravityDownHeaderStyleMemo}
                 >
                     {renderHeaderContent()}
                 </Animated.View>
             </Animated.View>
+            }
             <Animated.ScrollView
                 bounces={false}
                 ref={scrollViewRef}
                 snapToInterval={width}
                 onScroll={_onScrollHandler}
+                stickyHeaderIndices={allowStickyHeader}
                 {...props}
             >
-                <View style={[styles.scrollViewTopBox]} />
-                <Animated.View
-                    style={[
-                        styles.animatedViewRadius,
-                        {
-                            borderTopLeftRadius: borderRadiusValue,
-                            borderTopRightRadius: borderRadiusValue,
-                        },
-                        {
-                            backgroundColor: containerStyle?.backgroundColor || '#fff',
-                        },
-                    ]}
-                />
+                {!!image && <View style={styles.scrollViewTopBox} />}
+                {renderScrollHeader() && <Animated.View
+                    ref={headerRef}
+                    style={animatedHeaderStyleMemo}
+                >
+                    {renderScrollHeader()}
+                </Animated.View>}
                 <View style={containerStyle}>{children}</View>
             </Animated.ScrollView>
-        </>
+        </SafeAreaView>
     );
 };
 
@@ -135,12 +122,18 @@ const styles = StyleSheet.create({
         right: 0,
     },
     animatedViewRadius: {
-        height: 50,
+        minHeight: 50,
         width,
         backgroundColor: '#fff',
+        justifyContent: "center", alignItems: "center",
+        overflow: 'hidden',
     },
+    /**
+     * FIXME 70 is Hardcore height for ScrollView Header 
+     * looking for some help so this can be make dynamic for all iOS devices
+     **/
     scrollViewTopBox: {
-        height: width - 20,
+        height: width - (StatusBar.currentHeight || 70),
         width,
         backgroundColor: 'transparent',
     },
@@ -151,4 +144,44 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
     },
+    safeAreaView: {
+        flex: 1,
+    }
 });
+function mainHeaderImageStyle(imageStyle: ImageStyle, imageScale: Animated.AnimatedInterpolation) {
+    return StyleSheet.flatten([
+        imageStyle,
+        {
+            transform: [
+                {
+                    scale: imageScale,
+                },
+            ],
+        },
+    ]);
+}
+
+function gravityDownHeaderStyle(gravityDown: Animated.AnimatedInterpolation) {
+    return StyleSheet.flatten([
+        styles.renderHeaderContainer,
+        {
+            transform: [
+                {
+                    translateY: gravityDown,
+                },
+            ],
+        },
+    ]);
+}
+
+function animatedHeaderStyle(borderRadiusValue: Animated.AnimatedInterpolation, containerStyle: ViewStyle | undefined) {
+    return StyleSheet.flatten([
+        styles.animatedViewRadius,
+        {
+            borderTopLeftRadius: borderRadiusValue,
+            borderTopRightRadius: borderRadiusValue,
+            backgroundColor: containerStyle?.backgroundColor || '#fff',
+        }
+    ]);
+}
+
